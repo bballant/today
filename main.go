0cfbc6349c73a76c5034d7fca30a51395162911f
bballant <brianballantine@gmail.com>
2014-12-18 23:47:22 -0500

    you know, more stuff

--- main.go
+++ main.go
@@ -2,19 +2,37 @@ package main
 
 import (
 	"encoding/json"
-	"event"
 	"net/http"
-	"strconv"
+	//"strconv"
+	"fmt"
 	"time"
+
+	"appengine"
+	"appengine/user"
 )
 
-func main() {
-	http.HandleFunc("/hello", hello)
-	http.ListenAndServe(":8080", nil)
+func init() {
+	http.HandleFunc("/", handler)
 }
 
-func hello(w http.ResonseWriter, r *http.Request) {
-	g := Event{
-		Date: timeNow(),
+func handler(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	u := user.Current(c)
+	if u == nil {
+		url, err := user.LoginURL(c, r.URL.String())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Location", url)
+		w.WriteHeader(http.StatusFound)
 	}
+
+	g := Event{
+		Date: time.Now(),
+	}
+	fmt.Fprint(w, "Hello, world!\n")
+	var jj, _ = json.Marshal(g)
+	fmt.Fprintf(w, "%#v\n", g)
+	fmt.Fprintf(w, "%v", string(jj))
 }
